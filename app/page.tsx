69f3e64969eb5185f40eb9cc3de8141eb3ce8595
FedeCodeLab <federicoguzman.css@gmail.com>
2025-10-18 12:43:44 -0300

    Probando funcionalidad agregada a través de IA

--- app/page.tsx
+++ app/page.tsx
@@ -10,24 +10,26 @@ import { ToggleTheme } from "@/components/ui/ToggleTheme";
 
 export default function Home() {
   return (
-    <div className="container relative min-h-screen flex md:items-center justify-center">
+    <div className="container relative min-h-screen md:flex md:items-center justify-center">
       <ToggleTheme />
       <ApiWarning />
 
-      <div className="max-w-[90%] mx-auto md:mx-0 md:max-w-none rounded-[10px] bg-white dark:bg-neutral-900 md:min-w-full">
-        <section className="language-selection rounded-t-[10px] border border-highlight-neutral-300 dark:border-neutral-500">
-          <SourceLanguage />
+      <div className="w-full mx-auto flex justify-center items-center">
+        <div className="!mb-[20vh] md:!mt-[20vh] max-w-[90%] mx-auto md:mx-0 md:max-w-none rounded-[10px] bg-white dark:bg-neutral-900 md:min-w-full">
+          <section className="language-selection rounded-t-[10px] border border-highlight-neutral-300 dark:border-neutral-500">
+            <SourceLanguage />
 
-          <ButtonSwap />
+            <ButtonSwap />
 
-          <TargetLanguage />
-        </section>
+            <TargetLanguage />
+          </section>
 
-        <main className="flex flex-col md:flex-row h-[250px]">
-          <InputSection />
+          <main className="flex flex-col md:flex-row md:h-[250px]">
+            <InputSection />
 
-          <OutputSection />
-        </main>
+            <OutputSection />
+          </main>
+        </div>
       </div>
     </div>
   );
--- components/ui/ApiWarning.tsx
+++ components/ui/ApiWarning.tsx
@@ -1,10 +1,19 @@
+"use client";
+import { useEffect } from "react";
+import { useTranslatorStore } from "@/store/translatorStore";
+
 export const ApiWarning = () => {
+  const { hasNativeTranslator, hasNativeDetector, checkAPISupport } =
+    useTranslatorStore();
+
+  useEffect(() => {
+    checkAPISupport();
+  }, [checkAPISupport]);
+
+  if (hasNativeTranslator && hasNativeDetector) return null;
+
   return (
-    <div
-      id="apiWarning"
-      className="api-warning fixed left-10 bottom-10"
-      style={{ display: "none" }}
-    >
+    <div className="api-warning md:fixed md:left-10 md:bottom-10">
       ⚠️ APIs nativas de traducción no disponibles. Funcionalidad limitada.
     </div>
   );
